521865009ad0764f9c20e1b054867a8d0ac83f74
Raja Lenong <learning@rajalenong.com>
2023-11-27 01:58:52 +0700

    revisi assignment 1

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const app = Vue.createApp({
     data(){
         return{
-            yourName: 'Ahmad Husein',
-            yourAge: 27,
-            futureAge: 0
+            yourName: null,
+            yourAge: null
         }
     },
-    mounted(){
-        this.futureAge = this.yourAge + 5
-    },
+    // mounted(){
+    //     this.futureAge = this.yourAge + 5
+    // },
     methods: {
         favoriteNumber(){
             const randomNumber = Math.random();
             console.log(randomNumber);
-            if(randomNumber < 0,5){
+            if(randomNumber < 0.5){
                 return 1;
             } else {
                 return 0;
             }
+        },
+        futureAge(){
+            return parseInt(this.yourAge) + 5
         }
     }
 });
