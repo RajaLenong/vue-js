cdc92950821f3308362545947fc32e7ed485c4eb
Raja Lenong <learning@rajalenong.com>
2023-11-28 01:09:06 +0700

    adding code

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
 const app = Vue.createApp({
     data() {
       return {
-        counter: 10,
         name: '',
         confirmedName: ''
       }
     },
     methods: {
-        add(num) {
-            return this.counter = this.counter + num;
-        },
-        substract(num) {
-            return this.counter = this.counter - num;
-        },
-        setName(event, lastName) {
-            return this.name = event.target.value + ' ' + lastName;
+        setName() {
+            return this.name = event.target.value;
         },
         confirmedInput() {
             return this.confirmedName = this.name;
         },
         submitForm() {
-          alert('Submitted');
+          alert('any text of your choice');
         }
     }
   });
   
-  app.mount('#events');
+  app.mount('#assignment');
